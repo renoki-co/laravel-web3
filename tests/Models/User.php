29404d9f1d86264467733396f355b38b454d1966
rennokki <alex@renoki.org>
2021-03-30 16:17:43 +0300

    Initial commit

---
+++ tests/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RenokiCo\:package_namespace\Test\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
