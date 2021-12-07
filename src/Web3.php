b79f3ec9312fff9a36a884f2029535f5d8eefb2a
Alex Renoki <rennokki@gmail.com>
2021-12-07 19:42:44 +0200

    Added missing methods call

--- src/Web3.php
+++ src/Web3.php
@@ -5,6 +5,8 @@ namespace RenokiCo\LaravelWeb3;
 use Web3\Web3 as Web3Client;
 
 /**
+ * @method static string clientVersion()
+ * @method static string sha3(string $data)
  * @method \Web3\Namespaces\Db db()
  * @method \Web3\Namespaces\Eth eth()
  * @method \Web3\Namespaces\Net net()
--- src/Web3Facade.php
+++ src/Web3Facade.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static \RenokiCo\LaravelWeb3\Web3 connection(string $connection)
  * @method static \Web3\Web3 getClient()
+ * @method static string clientVersion()
+ * @method static string sha3(string $data)
  * @method static \Web3\Namespaces\Db db()
  * @method static \Web3\Namespaces\Eth eth()
  * @method static \Web3\Namespaces\Net net()
